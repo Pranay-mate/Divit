62b479bd44d9c078e122b09754486b62ca7e00d7
Pranay <matepranay11@gmail.com>
2020-08-14 20:04:39 +0530

    Updates

---
+++ precache-manifest.5f2aeaabcf1f0abe21bf0f6a1248ecd8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ffab97b3aded5316e267dc8a8b5a69",
+    "url": "/Divit/index.html"
+  },
+  {
+    "revision": "5a053d0ed9ce872ed404",
+    "url": "/Divit/static/css/2.d4b8f4cc.chunk.css"
+  },
+  {
+    "revision": "2db78cc227023ed6a569",
+    "url": "/Divit/static/css/main.96ba55c3.chunk.css"
+  },
+  {
+    "revision": "5a053d0ed9ce872ed404",
+    "url": "/Divit/static/js/2.919295d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Divit/static/js/2.919295d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db78cc227023ed6a569",
+    "url": "/Divit/static/js/main.747ba45e.chunk.js"
+  },
+  {
+    "revision": "7e510d59304ff4fab732",
+    "url": "/Divit/static/js/runtime-main.ef562173.js"
+  },
+  {
+    "revision": "97a1b7c803ab167626662b97c37c1c4b",
+    "url": "/Divit/static/media/Netflix.97a1b7c8.jpeg"
+  },
+  {
+    "revision": "d5fd6afdfe6ff11db50630549c96c60e",
+    "url": "/Divit/static/media/badminton.d5fd6afd.jpeg"
+  },
+  {
+    "revision": "3cafd7928f1e301c80ce58409be1302f",
+    "url": "/Divit/static/media/badminton1.3cafd792.jpg"
+  },
+  {
+    "revision": "5e3a2e8b1333d929b693dd55b4ec0300",
+    "url": "/Divit/static/media/dance.5e3a2e8b.jpeg"
+  },
+  {
+    "revision": "eaa7a38f23fb79f60299331bc4b3bed9",
+    "url": "/Divit/static/media/dance1.eaa7a38f.jpg"
+  },
+  {
+    "revision": "1fd8e06c5935a25345825c6fed38b03f",
+    "url": "/Divit/static/media/mine.1fd8e06c.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Divit/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Divit/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Divit/static/media/revicons.2feb69cc.eot"
+  }
+]);
