a7ef756ab5feeb4f578f2103b9bd5776d142544a
Pranay <matepranay11@gmail.com>
2020-08-15 18:12:45 +0530

    round link

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,11 +2,15 @@ import React from 'react';
 import mine from '../images/mine.jpg';
 import minew from '../images/minew.jpg';
 import mineg from '../images/mineg.jpg';
+import medium from '../images/medium.png';
+import linkedin from '../images/linkedin.png';
+import gmail from '../images/gmail.png';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import { TiSocialLinkedinCircular } from 'react-icons/ti';
+import { AiFillMediumCircle } from 'react-icons/ai';
 import Typist from 'react-typist';
 
 import '../css/AboutMe.css';
@@ -14,7 +18,7 @@ import '../css/AboutMe.css';
 function AboutMe() {
   return (
     <div className="AboutMe container-fluid ">
-  <Container>
+  <Container fluid>
       <Row>
          <Col xs={12} sm={4} className=' text-center'>
           <Image className='mine img-fluid' src={mineg} roundedCircle  alt='loading..' />
@@ -27,10 +31,10 @@ function AboutMe() {
         <Typist cursor={{ hideWhenDone: true }}>
         <span className=''>DevOps Engineer.</span>
         </Typist>
-        <h1 className=''>based in Mumbai, INDIA.</h1>
-        <a className='ml-1' href="https://www.linkedin.com/in/divitkarmiani/"><img src="https://img.icons8.com/ios/40/000000/linkedin.png" alt='loading..' /></a>
-        <a className='ml-1' href="mailto:divitkarmiani1998@gmail.com"><img src="https://img.icons8.com/ios/47/000000/gmail.png" alt='loading..' /></a>
-        <a className='ml-1' href="https://medium.com/@divitkarmiani1998"><img src="https://img.icons8.com/ios/40/000000/medium-monogram.png" alt='loading..' /></a>
+        <h1 className=''>based in Mumbai.</h1>
+        <a className='ml-1' href="https://www.linkedin.com/in/divitkarmiani/"><img src={linkedin} height='50px'  alt='loading..' /></a>
+        <a className='ml-1' href="mailto:divitkarmiani1998@gmail.com"><img src={gmail} height='50px'  alt='loading..' /></a>
+        <a className='ml-1' href="https://medium.com/@divitkarmiani1998"><img src={medium} height='50px'  alt='loading..' /></a>
 
         </div>
 
