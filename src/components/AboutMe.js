7670ba9a98ef87a77146b7307869b81884c4e701
Pranay <matepranay11@gmail.com>
2020-08-24 16:56:27 +0530

    experience title

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -47,8 +47,7 @@ class Skills extends React.Component {
   <Card.Body>
 
       <div className=''>
-    <p> If you ask his friends, they will say he is the one to keep close and that they wouldn’t change a thing about him. His goal in life is to fulfill the meaning of his name – “Immortal”. And Product management can help him achieve that. The impact of products can reach a wide array of audience and can make a small yet significant difference to someone’s life. He dearly wants to succeed in doing that. And even if he has just started the race, he is on the right course.</p>
-    <p> I can go on and on about him, but I think that’s enough about Divit, let’s be modest. Ahh like Divit is of course!</p>
+    <p>If you ask his friends, they will say he is the one to keep close and that they wouldn’t change a thing about him. His goal in life is to fulfill the meaning of his name – “Immortal”. And Product management can help him achieve that. The impact of products can reach a wide array of audience and can make a small yet significant difference to someone’s life. Even though he has just started the race, he is on the right track and he aspires to win it too someday with his determination.</p>
 
                 </div>
                 </Card.Body>
